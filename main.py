74208d9419c6e15ea5c844a4747973d7dda425cb
Aluriak <lucas.bourneuf@laposte.net>
2022-02-10 13:07:38 +0100

    currently start the first found service

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 import sys
+import glob
 from poc import create_app
 from utils import create_sorry_app
 
-app = create_app(sys.argv[1]) or create_sorry_app()
+cfg = next(iter(glob.glob('services/*.json')), None)
+app = cfg and create_app(cfg) or create_sorry_app()
