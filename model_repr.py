d1f17a48f4d695ffd81ae7af393ace5ab3a4c310
Aluriak <lucas.bourneuf@laposte.net>
2022-02-13 19:50:19 +0100

    model repr now uses the plugin system to dynamically discover and use the available representation plugins

--- model_repr.py
+++ model_repr.py
@@ -1,75 +1,25 @@
-"""Functions creating html representation of ASP models.
+"""Functions loading the plugins, and exposing all functions to
+create html representation out of ASP models.
+
 """
+from pluginsystem import ModelReprPlugin
 
 
-def from_name(repr_name: str) -> callable:
-    "Return the representation function corresponding to given representation name"
-    return FUNCS[repr_name]
+def gen_model_repr_plugins(repr_options_list: list[dict], get_username_of: callable, get_choicename_of: callable) -> [ModelReprPlugin]:
+    "Yield the ordered list of representation functions asked by given configuration"
+    for rule in repr_options_list:
+        repr_name = rule['kind']
+        if repr_name not in MODEL_REPR_PLUGINS:
+            assert False, "that shouldn't happen in configuration was properly verified"
+        yield MODEL_REPR_PLUGINS[repr_name](rule, get_username_of, get_choicename_of)
 
-
-def names() -> [str]:
+def names() -> frozenset[str]:
     "Return the available representation names"
-    return tuple(FUNCS.keys())
+    return frozenset(MODEL_REPR_PLUGINS)
+
+def get_plugin_by_name(name: str) -> ModelReprPlugin:
+    return MODEL_REPR_PLUGINS[name]
 
 
-def model_stable_repr(model: frozenset) -> tuple:
-    """Return the same model, only everything is ordered so that models
-    with exact same atoms get the exact same representation.
-
-    This is used to ensure that each model is its own and unchanged identifier
-    between two encoding compilation. Its representation change only
-    if the atoms changes.
-
-    """
-    if isinstance(model, frozenset) and all(isinstance(e, tuple) and isinstance(e[0], str) and isinstance(e[1], tuple) for e in model):
-        # model is frozenset of (predicate:str, args:tuple)
-        # args are (since their order is data) left untouched.
-        return tuple(sorted(list(model)))
-    else:
-        raise ValueError(f"Received model of type {type(model)} cannot be transformed into a stable representation: {model}")
-
-
-def as_raw(idx: int, model: tuple, userid_to_label: callable, choiceid_to_label: callable, uid: str = None) -> str:
-    """Return html representation of given model"""
-
-    by_pred = {}
-    for atom, args in model:
-        by_pred.setdefault(atom, []).append(args)
-    html = []
-    for atom, argss in by_pred.items():
-        html.append(f'{len(argss)} <code>{atom}</code> atoms found: <code>' + ' '.join(f'{atom}({",".join(map(str, args))}).' for args in argss) + '</code>')
-    # if only one atom of arity 2, then show it as a table
-    if len(by_pred) == 1 and all(len(args) == 2 for args in next(iter(by_pred.values()))):
-        html.append('')
-        html.append(as_table(idx, model, userid_to_label, choiceid_to_label, integrated=True, uid=None))
-    uid_repr = f'<b>— {uid} —</b><br/><br/>' if uid else ''
-    return f'<h2>Solution {idx}</h2>{uid_repr}<br/>' + '<br/>'.join(html) + '<br/>'
-
-
-def as_table(idx: int, model: tuple, obj_to_label: callable, att_to_label: callable, integrated: bool = False, uid: str = None) -> str:
-    """Return html representation of given model"""
-
-    objs, atts, rels = set(), set(), {}
-    for atom, args in model:
-        if len(args) == 2:
-            obj, att = args
-            objs.add(obj)
-            atts.add(att)
-            rels.setdefault(obj, set()).add(att)
-    html = [' <tr>\n  <td></td>\n' + ''.join(f'   <th>{att_to_label(att)}</th>\n' for att in atts) + ' </tr>\n']
-    for obj in objs:
-        html.append(f' <tr>\n  <td>{obj_to_label(obj)}</td>' + ''.join(f'   <td>{"×" if att in rels[obj] else ""}</td>\n' for att in atts) + ' </tr>\n')
-
-    html = '<table>' + ''.join(html) + '</table>'
-    unique_name_repr = f'<br/><b>— {uid} —</b><br/>' if uid else ''
-
-    if integrated:
-        return html + unique_name_repr
-    else:
-        return f'<h2>Solution {idx}</h2><br/>{html}{unique_name_repr}<br/>'
-
-
-FUNCS = {
-    'raw': as_raw,
-    'table/2': as_table,
-}
+MODEL_REPR_PLUGINS = ModelReprPlugin.get_plugins()
+print(MODEL_REPR_PLUGINS)
