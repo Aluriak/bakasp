737a71a02f6cea1b9c1bf5b42215bbf71491f984
Aluriak <lucas.bourneuf@laposte.net>
2022-03-31 14:54:50 +0200

    allow to specify an admin code

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ def readable_file(path:str) -> str:
 def parse_cli() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('configuration', nargs='?', type=readable_file, help='configuration to run', default='examples/choose-your-character.json')
+    parser.add_argument('--admin-code', '-a', type=str, help='admin code, if any', default=None)
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_cli()
     cfg = args.configuration
-    app = cfg and create_app(cfg) or create_sorry_app('No configuration found.')
+    app = cfg and create_app(cfg, admin=args.admin_code) or create_sorry_app('No configuration found.')
     app.run(port=8080, debug=True)
