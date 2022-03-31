e6e532c9563a83571a52dff38d40ab37b11e76c9
Aluriak <lucas.bourneuf@laposte.net>
2022-03-31 14:23:40 +0200

    main is a runner for any given configuration

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+"""Example of bakasp instance creation from python"""
 
-import glob
-from poc import create_app
+import os
+import argparse
+from bakasp import create_app
 from utils import create_sorry_app
 
-cfg = next(iter(glob.glob('services/*.json')), None)
-app = cfg and create_app(cfg) or create_sorry_app('No configuration found.')
+
+def readable_file(path:str) -> str:
+    """Argparse type, raising an error if given path is not pointing to a readable file"""
+    if not os.path.isfile(path):
+        raise argparse.ArgumentTypeError(f"No such file: {path}")
+    # all testable files are here
+    return path
+
+def parse_cli() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('configuration', nargs='?', type=readable_file, help='configuration to run', default='examples/choose-your-character.json')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_cli()
+    cfg = args.configuration
+    app = cfg and create_app(cfg) or create_sorry_app('No configuration found.')
+    app.run(port=8080, debug=True)
