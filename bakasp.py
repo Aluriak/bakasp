ced63768758393298f74a415de3fcfb2f582a057
Aluriak <lucas.bourneuf@laposte.net>
2022-03-31 14:36:40 +0200

    use the new bakend API

--- bakasp.py
+++ bakasp.py
@@ -24,48 +24,7 @@ def create_website(cfg: dict, raw_cfg: dict, *, admin: str = None):
     template_folder = os.path.join('templates/', cfg['global options']['template'])
     app = Flask(__name__, template_folder=template_folder)
     back = Backend('', admin, cfg, raw_cfg)
-
-    users_who_changed_their_choices = set()
-    stats = {}  # some stats about global state
-    models = []  # list of all found models
-    result_header, result_footer = '', ''  # header and footer of the result page
-    history = []  # (datetime, userids -> choices, new_models, lost_models)
-    user_choices = {}  # userid -> choices
-    previous_models_uid = set()  # uids of found models before last compilation
-
-
-    app.route('/')(back.html_instance_page)
-    app.route('/thanks')(back.html_thank_you_page)
-
-    app.route('/user')(back.html_user_list_page)
-
-    @app.route('/user/<userid>', methods=['GET', 'POST'])
-    def page_user_choice(userid):
-        if request.method == 'POST':
-            return back.set_user_choice(userid, request.form)
-        else:
-            return back.html_user_choice_page(userid)
-
-
-    if 'configuration' in cfg["global options"]["public pages"]:
-        app.route('/configuration')(back.html_config)
-        app.route('/configuration/raw')(back.html_raw_config)
-
-    if 'compilation' in cfg["global options"]["public pages"]:
-        app.route('/compilation')(back.html_compilation)
-
-    if 'history' in cfg["global options"]["public pages"]:
-        app.route('/history')(back.html_history)
-
-    if 'overview' in cfg["global options"]["public pages"]:
-        app.route('/overview')(back.html_overview)
-
-    if 'results' in cfg["global options"]["public pages"]:
-        app.route('/results')(back.html_results)
-
-    if 'reset' in cfg["global options"]["public pages"] and cfg['meta']['save state']:
-        app.route('/reset')(back.html_reset)
-
+    back.link_to_flask_app(app)
     return app
 
 
