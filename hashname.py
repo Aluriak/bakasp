60ab83415e7215ba968161ef381036c2cc98060d
Lucas Bourneuf <lucas@bourneuf.net>
2023-04-03 01:29:05 +0200

    implement collision test ; looks like there is a bias toward some combinations

--- hashname.py
+++ hashname.py
@@ -14,9 +14,8 @@ and updated with other words i found relevant.
 import uuid
 import json
 import hashlib
+import itertools
 from utils import by_chunks
-
-WORD_SIZE=6
 
 
 def as_json_object(obj: object) -> object:
@@ -47,21 +46,34 @@ try:
 except:
     NOUNS = ['jean', 'horse', 'invasion', 'rock', 'nitrite', 'vessel', 'base', 'leaf', 'thief', 'atom', 'well', 'saint', 'purity', 'velvet', 'unicorn', 'santa', 'yodeler', 'micro', 'dragon', 'spouse', 'night', 'sun', 'tree', 'dog', 'polder', 'heart', 'bean', 'turtle', 'battery', 'archetype', 'trope', 'mustache', 'paper', 'pine', 'luck', 'shock', 'cat', 'eye', 'paw', 'course', 'computer', 'predator', 'bus', 'apple', 'jewelry', 'spice', 'wings', 'submarine', 'juice', 'garden', 'rain', 'nose', 'plane', 'number', 'watchtower', 'cube', 'window', 'burger', 'sheep', 'driver', 'ocean', 'book', 'mountain', 'dress', 'god', 'ritual', 'light', 'lantern', 'queen', 'bear', 'office', 'shield', 'waters', 'lead', 'cloud', 'roof', 'sunday', 'monday', 'scream', 'pepper']
 
-def as_noun(nb: str) -> str:
-    return NOUNS[int(''.join(nb)) % len(NOUNS)]
 
-def as_adj(nb: str) -> str:
-    return ADJECTIVES[int(''.join(nb)) % len(ADJECTIVES)]
+from collections import defaultdict
+FOUND_NOUNS = defaultdict(int)
+FOUND_ADJS = defaultdict(int)
+
+def as_noun(nb: str, NOUNS=NOUNS) -> str:
+    nb = int(''.join(nb))
+    # print('NOUN:', nb, '->', nb % len(NOUNS))
+    FOUND_NOUNS[nb % len(NOUNS)] += 1
+    return NOUNS[nb % len(NOUNS)]
+
+def as_adj(nb: str, ADJECTIVES=ADJECTIVES) -> str:
+    nb = int(''.join(nb))
+    # print('ADJC:', int(''.join(nb)), '->', int(''.join(nb)) % len(ADJECTIVES))
+    FOUND_ADJS[nb % len(NOUNS)] += 1
+    return ADJECTIVES[nb % len(ADJECTIVES)]
 
 
 
 def get_random_hash() -> str:
     return from_obj(str(uuid.uuid4()))
 
-def from_obj(obj: object, nb_chunks: int = 2, **kwargs) -> str:
-    h = hashlib.blake2b(digest_size=nb_chunks*WORD_SIZE, usedforsecurity=False)
+def from_obj(obj: object, nb_chunks: int = 2, word_size: int = 6, **kwargs) -> str:
+    h = hashlib.blake2b(digest_size=nb_chunks*word_size, usedforsecurity=False)
     h.update(as_bytes(as_json_object(obj)))
     final_h = ''.join(str(val).ljust(3, '0') for val in h.digest())
+    # (each val of h.digest is a byte (0-255), hence .ljust it allows to always get 3 digits per val.)
+    assert (len(final_h) / 3) == (nb_chunks * word_size), final_h
     return __from_hash(final_h, nb_chunks=nb_chunks, **kwargs)
 
 
@@ -70,17 +82,67 @@ def __from_hash(h: str, nb_chunks: int, joiner: str=' ', style=str.title) -> str
         # h += '0'
     rest = len(h) % nb_chunks
     assert rest == 0, rest
+    assert h.isnumeric(), h
     chunk_size = len(h) // nb_chunks
+    structure = itertools.cycle([as_adj, as_noun])
 
-    if nb_chunks == 2:
-        one, two = by_chunks(h, chunk_size)
-        cmps = as_adj(one), as_noun(two)
-
-    elif nb_chunks == 3:
-        one, two, tee = by_chunks(h, chunk_size)
-        cmps = as_adj(one), as_noun(two), as_adj(tee)
+    cmps = (adj_or_noun(chunk) for adj_or_noun, chunk in zip(structure, by_chunks(h, chunk_size)))
+    # print('t:', tuple(adj_or_noun(chunk) for adj_or_noun, chunk in zip(structure, by_chunks(h, chunk_size))))
 
     return style(joiner.join(cmps))
+
+
+
+def run_collision_test():
+    from collections import Counter
+    SIZE = 100000
+    c = Counter(from_obj(i) for i in range(SIZE))
+    collisions = {k: v for k, v in c.items() if v > 1}
+    print(f'collision: occurences (of {SIZE})')
+    for k, v in sorted(tuple(Counter(collisions.values()).items())):
+        print(f'\t{k}: {v} ({round(v/SIZE*100, 2)}%)')
+
+    print(f'Total number of collisions: {sum(collisions.values())} ({round(sum(collisions.values())/SIZE*100, 2)}% of {SIZE})')
+    print(f'Number of different hashes: {len(NOUNS) * len(ADJECTIVES)} ({round(len(NOUNS) * len(ADJECTIVES) / SIZE * 100, 2)}% of {SIZE})')
+    print(f'Theoretical minimal number of collisions: {max(0, SIZE - len(NOUNS) * len(ADJECTIVES))} ({max(0, SIZE - len(NOUNS) * len(ADJECTIVES)) / SIZE * 100}% of {SIZE})')
+
+    print(f'Number of used nouns: {len(FOUND_NOUNS)} ({round(len(FOUND_NOUNS) / len(NOUNS) * 100, 2)}% of {len(NOUNS)})')
+    print(f'Number of used adjectives: {len(FOUND_ADJS)} ({round(len(FOUND_ADJS) / len(ADJECTIVES) * 100, 2)}% of {len(ADJECTIVES)})')
+
+
+
+
+    nouns, nouns_occs = zip(*sorted(list(FOUND_NOUNS.items())))
+    adjs, adjs_occs = zip(*sorted(list(FOUND_ADJS.items())))
+
+
+    import statistics as stats
+    print(f'Nouns Occurences:   mean: {round(stats.mean(nouns_occs), 2)}')
+    print(f'                  pstdev: {round(stats.pstdev(nouns_occs), 2)}')
+
+    print(f'Adjcs Occurences:   mean: {round(stats.mean(adjs_occs), 2)}')
+    print(f'                  pstdev: {round(stats.pstdev(adjs_occs), 2)}')
+
+
+
+    import seaborn as sns
+    from pandas import DataFrame as df
+    import matplotlib.pyplot as plt
+
+
+    data_nouns = df({'noun': nouns, 'occs': nouns_occs})
+    data_adjs = df({'adj': adjs, 'occs': adjs_occs})
+    ax = sns.relplot(data=data_nouns, x='noun', y="occs")
+    ax.set(title='Nouns')
+    ax = sns.catplot(data=data_nouns, y="occs", kind='violin')
+    ax.set(title='Nouns')
+    ax = sns.relplot(data=data_adjs, x='adj', y="occs")
+    ax.set(title='Adjectives')
+    ax = sns.catplot(data=data_adjs, y="occs", kind='violin')
+    ax.set(title='Adjectives')
+    plt.show()
+
+
 
 
 
@@ -88,5 +150,4 @@ def __from_hash(h: str, nb_chunks: int, joiner: str=' ', style=str.title) -> str
 if __name__ == "__main__":
 
     import random
-    # for _ in range(10):
     run_collision_test()
